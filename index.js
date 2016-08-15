7051616c93c8c626062768a641e25ce56d1310ba
haishanh <haishanhan@gmail.com>
2016-08-15 10:37:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var spawnSync = require('child_process').spawnSync;
+
+var cmd = 'lib/deploy';
+
+function deploy(opt) {
+  if (opt && opt.directory && opt.remote && opt.branch)  {
+    var args = ['-d', opt.directory, '-r', opt.remote, '-b', opt.branch];
+    spawnSync(cmd, args, {stdio: 'inherit'});
+  } else {
+    console.log('Please supply all needed option properties');
+  }
+}
+
+module.exports = deploy;
